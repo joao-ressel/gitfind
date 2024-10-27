55c2a617b577a0a1a593cb7c6b20b23f52d867c1
joao-ressel <joao.resselti@gmail.com>
2024-10-27 15:02:52 -0500

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,8 @@
+import './styles.css';
+export const Header = ()=>{
+    return(
+        <header>
+            <h1>GitFind</h1>
+        </header>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import {Home} from './pages/Home'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Home />
+  </StrictMode>,
+)
+
