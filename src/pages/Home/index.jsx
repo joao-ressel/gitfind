9afcec0adfd7fbae6759f1bbfcdd5a2c02a0d93b
joao-ressel <joao.resselti@gmail.com>
2024-10-27 15:23:52 -0500

    adicionando formulario de busca

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,19 @@
-import { Header } from "../../components/Header"
-
-export const Home = ()=>{
-    return(
-        <>
-        <Header/>
-        </>
-    )
-}
+import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
+import "./styles.css";
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className="conteudo">
+        <img src={background} alt="" className="background" />
+     <div className="info">
+        <form>
+            <input type="text" name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+        </form>
+     </div>
+      </div>
+    </>
+  );
+};
